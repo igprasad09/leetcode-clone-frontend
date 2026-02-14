c4f7bf21ef4b78967c4e452c60e05536520232d3
Prasad Kamble <141195985+igprasad09@users.noreply.github.com>
2026-02-14 07:48:32 +0530

    Update BackgroundBeams component with comments and styles

--- src/components/ui/background-beams.tsx
+++ src/components/ui/background-beams.tsx
@@ -5,7 +5,7 @@ import { cn } from "@/lib/utils";
 
 export const BackgroundBeams = React.memo(
   ({ className }: { className?: string }) => {
-    //checking
+    //commit
     const paths = [
       "M-380 -189C-380 -189 -312 216 152 343C616 470 684 875 684 875",
       "M-373 -197C-373 -197 -305 208 159 335C623 462 691 867 691 867",
@@ -61,9 +61,9 @@ export const BackgroundBeams = React.memo(
     return (
       <div
         className={cn(
-          "absolute inset-0 flex h-full w-full items-center justify-center [mask-repeat:no-repeat] [mask-size:40px]",
-          className,
-        )}
+    "absolute inset-0 flex h-full w-full items-center justify-center [mask-repeat:no-repeat] [mask-size:40px] pointer-events-none",
+    className,
+  )}
       >
         <svg
           className="pointer-events-none absolute z-0 h-full w-full"
